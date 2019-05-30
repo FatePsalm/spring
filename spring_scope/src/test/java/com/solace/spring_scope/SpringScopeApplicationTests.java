468255ee52ca5115946e1a0ef910cb8f13d971d5
Solace <545058656@qq.com>
2019-05-30 17:35:50 +0800

    2

--- spring_scope/src/test/java/com/solace/spring_scope/SpringScopeApplicationTests.java
+++ spring_scope/src/test/java/com/solace/spring_scope/SpringScopeApplicationTests.java
@@ -5,9 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.swing.*;
+import java.awt.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringScopeApplicationTests {
+
 
     @Test
     public void contextLoads() {
